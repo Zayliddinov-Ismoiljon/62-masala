27ee5a91d70deb786711979f2593ff5b452c9e83
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2022-03-25 06:40:29 +0500

    Innitial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Masala: n ta elementdan tashkil topgan massiv berilgan indexlari kamayib borish tartibida chiqaruvchi dastur tuzing
+
+let n=9;
+let a=[];
+
+for(i=n; i>=0; i--){
+    a.push(i);
+}
+
+console.log(a);
